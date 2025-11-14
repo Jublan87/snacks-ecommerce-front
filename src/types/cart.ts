04c1268a7456a2ae1056086794058a3539dc69e7
Blanco Caruso Juan <Juan.Blanco@externos.afpcapital.cl>
2025-11-14 16:07:23 -0300

    Se implementa Zustand Store

---
+++ src/types/cart.ts
@@ -0,0 +1,13 @@
+import { Product } from './product';
+
+export interface CartItem {
+  id: string;
+  product: Product;
+  quantity: number;
+  addedAt: string;
+}
+
+export interface Cart {
+  items: CartItem[];
+  updatedAt: string;
+}
